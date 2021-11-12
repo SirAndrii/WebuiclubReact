0bbedd9fb95210782365b536c60bf8f758b4e3c7
Andrii Sirotenko <andruhe@gmail.com>
2021-11-12 23:20:23 +0200

    small fixes

--- src/components/main-articles.js
+++ src/components/main-articles.js
@@ -8,17 +8,15 @@ export default function Articles () {
     const [loadMore, setLoadMore] = useState(step);
     //Чому воно працює не дуже розумію, передаєм як колбек функцію, але в той же час маєм доступ до батьківського стейту 
         const handleEvent = () => {
-        setLoadMore(loadMore+step);
-        console.log(loadMore);
+            setLoadMore(loadMore+step);
+            console.log(loadMore);
     }
 
     let articles = objArticles.slice(0,loadMore);
 
 return (
     <div className="row">
-        
-        {articles.map((el) => <Article title={el.title} img={el.img}/> )}
-
+        {articles.map((el) => <Article key={el.title} title={el.title} img={el.img}/> )}
         <ViewMore onclick={handleEvent}/>
     </div>
 );
