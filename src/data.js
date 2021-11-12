0f5bde8a3aff8b5396656e0ead3f3148975a46b1
Andrii Sirotenko <andruhe@gmail.com>
2021-11-12 16:28:24 +0200

    moved object with articles data to external file data.js

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const objArticles = [
+    {
+        title: 'Title 1',
+        img: 'https://miro.medium.com/max/3600/0*n-2bW82Z6m6U2bij.jpeg'
+    },
+    {
+        title: 'Title 2',
+        img: 'https://pbs.twimg.com/media/B5pjF_8IEAE_erU.jpg'
+    },
+    {
+        title: 'Title 3',
+        img: 'https://78.media.tumblr.com/7038fa86c7c2010178ed41a23ac358e2/tumblr_ohkxh5QI181tjftelo1_1280.jpg'
+    },
+      {
+        title: 'Title 4',
+        img: 'https://miro.medium.com/max/3600/0*n-2bW82Z6m6U2bij.jpeg'
+    },
+    {
+        title: 'Title 5',
+        img: 'https://pbs.twimg.com/media/B5pjF_8IEAE_erU.jpg'
+    },
+    {
+        title: 'Title 6',
+        img: 'https://78.media.tumblr.com/7038fa86c7c2010178ed41a23ac358e2/tumblr_ohkxh5QI181tjftelo1_1280.jpg'
+    },
+      {
+        title: 'Title 7',
+        img: 'https://miro.medium.com/max/3600/0*n-2bW82Z6m6U2bij.jpeg'
+    },
+    {
+        title: 'Title 8',
+        img: 'https://pbs.twimg.com/media/B5pjF_8IEAE_erU.jpg'
+    },
+    {
+        title: 'Title 9',
+        img: 'https://78.media.tumblr.com/7038fa86c7c2010178ed41a23ac358e2/tumblr_ohkxh5QI181tjftelo1_1280.jpg'
+    }
+]
