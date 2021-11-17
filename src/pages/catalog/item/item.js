6bdba4a220fe37b3ac419a375e900493da8be73e
Andrii Sirotenko <andruhe@gmail.com>
2021-11-17 13:11:15 +0200

    add dynamic routing to item Page

--- src/pages/catalog/item/item.js
+++ src/pages/catalog/item/item.js
@@ -1,10 +1,23 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, {useState} from "react";
+import { useParams, useNavigate  } from "react-router-dom";
+// useHistory was canceled  in V6"react-router-dom"
+import Button from "../../../components/button";
 
 export default function Item() {
+    const [redirect,setRedirect] = useState(null)
+    
     let params = useParams();
+    let navigate = useNavigate();
+    
+
+//Якщо юзер буде вбивати що завгодно, всеодно сторінка буда ренедеритись. Зробити кондішенал редірект в мене не вийшло.
+
     let id = params.item.match(/(\d+)/)[0];
+    
     return (
-        <h2>{params.item} - id = {+id}</h2>
+        <>
+        <h2>{params.item} - id={+id}</h2>
+        <Button classname="btn-large" onclick={()=> navigate(-1)}>Back to catalog</Button>
+        </>
     );
 }
