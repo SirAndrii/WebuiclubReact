e31e7eb653a8018e724e1c3bf4f04a738891e664
Andrii Sirotenko <andruhe@gmail.com>
2021-11-18 10:14:14 +0200

    made menu flexible again

--- src/components/header.js
+++ src/components/header.js
@@ -8,10 +8,10 @@ const Header = () => {
   return (
     <header>
         <nav>
-            <div className="nav-wrapper" style={{display:'flex', flexDirection: 'row'}}>
+            <div className="nav-wrapper" style={{display:'flex', flexDirection: 'row', justifyContent:'space-between'}}>
               <Link to="/"><Logo /></Link>
               <Menu />
-              { useLocation().pathname==='/catalog' && <Search />}
+              { useLocation().pathname==='/catalog' ? <Search /> : <div style={{width:'180px'}}></div>}
             </div>
         </nav>
     </header>
